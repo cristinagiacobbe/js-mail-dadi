46f0dc8233101923146bf2213c1d751ba451516d
cristinagiacobbe <crigiak@gmail.com>
2023-11-26 10:44:31 +0100

    define tools

---
+++ scritpt.js
@@ -0,0 +1,10 @@
+//chiedi all'utente la sua email
+//controlla che sia nella lista di chi può accedere
+//stampa un messaggio sull'esito del controllo
+
+//TOOLS
+//prompt to aske user his email
+//list with mail addresses
+//check cicle (for/if)
+//inner.text to print check result
+
